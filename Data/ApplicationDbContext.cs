2144ca21092afc565eb4cbfa9d95ae1ba9bb99ad
KenneMN3 <Kexxu10@gmail.com>
2025-03-12 18:22:43 -0500

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Biblioteca.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biblioteca.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+   : base(options)
+    {
+    }
+    public DbSet<LibrosModels> Libros { get; set; }
+    public DbSet<AutoresModels> Autores { get; set; }
+
+}
---
+++ Data/Migrations/20250312001739_tablas.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Biblioteca.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class tablas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "FechaNacimiento",
+                table: "Autores",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "FechaNacimiento",
+                table: "Autores",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+    }
+}

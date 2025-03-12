de7e39febd389dc2dcf00293f48752e7d07d460b
KenneMN3 <Kexxu10@gmail.com>
2025-03-11 19:23:16 -0500

    Agregar archivos de proyecto.

---
+++ Models/AutoresModels.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Biblioteca.Models
+{
+    public class AutoresModels
+    {
+        [Key]
+        public int AutorId { get; set; }  // Primary Key
+
+        [Required(ErrorMessage = "El nombre del autor es requerido.")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El apellido del autor es requerido.")]
+        public string Apellido { get; set; }
+
+        [Required(ErrorMessage = "La fecha de nacimiento del autor es requerida.")]
+        [DataType(DataType.Date)]  // Asegura que solo se almacene la fecha, no la hora.
+        public DateTime FechaNacimiento { get; set; }
+
+        [Required(ErrorMessage = "La nacionalidad del autor es requerida.")]
+        public string Nacionalidad { get; set; }
+
+        ///// Relaciones con Libros
+        public ICollection<LibrosModels> Libros { get; set; }  // Relación 1:N con Libros
+    }
+}
---
+++ Models/LibrosModels.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Biblioteca.Models
+{
+    public class LibrosModels
+    {
+        [Key]
+        public int LibroId { get; set; }  // Primary Key
+
+        [Required(ErrorMessage = "El título del libro es requerido.")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "El género del libro es requerido.")]
+        public string Genero { get; set; }
+
+        [Required(ErrorMessage = "La fecha de publicación es requerida.")]
+        [DataType(DataType.Date)]
+        public DateOnly FechaPublicacion { get; set; }
+
+        [Required(ErrorMessage = "El ISBN es requerido.")]
+        public string ISBN { get; set; }
+
+        ///// Relaciones con Autor
+        [ForeignKey("AutorId")]
+        public int AutorId { get; set; }  // Foreign Key a AutoresModels
+
+        public AutoresModels Autor { get; set; }  // Relación N:1 con Autores
+    }
+}
